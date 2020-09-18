da4fa1cebb9c4db35ab01f1fdd57faecbef0dcc7
Michael J Conrad <32316111+Siphonophora@users.noreply.github.com>
2020-09-17 20:38:42 -0400

    Remove all files

--- src/tests/SqlDataCompare.UnitTests/FxCopProjectSuppressions.cs
+++
@@ -1,3 +0,0 @@
-﻿using System.Diagnostics.CodeAnalysis;
-
-[assembly: SuppressMessage("Naming", "CA1707:Identifiers should not contain underscores", Justification = "Allow Underscores in Unit Test Names")]
